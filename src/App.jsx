2fcd2a3f856f769e327d759d425ce9effaa1021c
Stephan Danser <stephandanser@macbookpro.home>
2025-10-25 13:20:52 +0200

    app adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import CrooksLegends from "./pages/CrooksLegends";
 import EmpireProfile from "./pages/EmpireProfile";
 import EmpireBank from "./pages/EmpireBank.jsx";
 import EmpireHeists from "./pages/EmpireHeists.jsx";
-import AdminPanel from "./pages/AdminPanel.jsx";  // ⬅️ new
+import AdminPanel from "./pages/AdminPanel.jsx";
 import { WalletProvider } from "./context/WalletContext";
+import { EmpireProvider } from "./context/EmpireContext";   // ✅ import this
 
 // tijdelijk debuggen – mag je later verwijderen
 console.log("ENV DEBUG:", {
@@ -21,27 +22,24 @@ console.log("ENV DEBUG:", {
 export default function App() {
   return (
     <WalletProvider>
-      <BrowserRouter>
-        <Navbar />
-        {/* ruimte onder de fixed navbar */}
-        <div className="pt-16">
-          <Routes>
-            <Route path="/" element={<CrooksFinance />} />
-            <Route path="/legends" element={<CrooksLegends />} />
-            <Route path="/empire" element={<CrooksEmpire />} />
-            <Route path="/empire/armory" element={<EmpireArmory />} />
-            <Route path="/empire/profile" element={<EmpireProfile />} />
-            <Route path="/empire/bank" element={<EmpireBank />} />
-            <Route path="/empire/heists" element={<EmpireHeists />} />
-
-            {/* 🔒 ADMIN PANEL route */}
-            <Route path="/admin" element={<AdminPanel />} />
-
-            {/* stubs so tiles don’t 404 */}
-            <Route path="/empire/casino" element={<div />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <EmpireProvider> {/* ✅ add this wrapper */}
+        <BrowserRouter>
+          <Navbar />
+          <div className="pt-16">
+            <Routes>
+              <Route path="/" element={<CrooksFinance />} />
+              <Route path="/legends" element={<CrooksLegends />} />
+              <Route path="/empire" element={<CrooksEmpire />} />
+              <Route path="/empire/armory" element={<EmpireArmory />} />
+              <Route path="/empire/profile" element={<EmpireProfile />} />
+              <Route path="/empire/bank" element={<EmpireBank />} />
+              <Route path="/empire/heists" element={<EmpireHeists />} />
+              <Route path="/admin" element={<AdminPanel />} />
+              <Route path="/empire/casino" element={<div />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </EmpireProvider>
     </WalletProvider>
   );
 }
